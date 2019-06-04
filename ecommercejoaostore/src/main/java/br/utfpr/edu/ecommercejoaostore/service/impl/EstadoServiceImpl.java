6650e4afe49af660f9197c68c81d169a163a0211
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2019-06-04 20:04:03 -0300

    Mapeadas as entidades de Marca e Permissão.

--- ecommercejoaostore/src/main/java/br/utfpr/edu/ecommercejoaostore/service/impl/EstadoServiceImpl.java
+++ ecommercejoaostore/src/main/java/br/utfpr/edu/ecommercejoaostore/service/impl/EstadoServiceImpl.java
@@ -2,11 +2,15 @@ package br.utfpr.edu.ecommercejoaostore.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import br.utfpr.edu.ecommercejoaostore.model.Estado;
 import br.utfpr.edu.ecommercejoaostore.repository.EstadoRepository;
+import br.utfpr.edu.ecommercejoaostore.service.EstadoService;
 
-public class EstadoServiceImpl extends CrudServiceImpl<Estado, Integer> {
+@Service
+public class EstadoServiceImpl extends CrudServiceImpl  <Estado, Integer>
+		implements EstadoService{
 
 	@Autowired
 	private EstadoRepository estadoRepository;
--- ecommercejoaostore/src/main/resources/static/js/crud-estado.js
+++ ecommercejoaostore/src/main/resources/static/js/crud-estado.js
@@ -14,23 +14,3 @@ function formatDate(inputFormat){
 	return [ pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear() ].join('/');
 }
 
-
-function saveUpload(urlDestino){
-	var formData = new FormData($('#frm')[0]);
-	$.ajax({
-		type: $('#frm').attr('method'),
-		 url: $('#frm').attr('action'),
-		data: formData,
-		async: false,
-		cache: false,
-		contentType: false,
-		processData: false,
-		success: function(){
-			swal('Salvo!', 'Registro salvo com sucesso!', 'success');
-			window.location = urlDestino;
-		},
-		error: function(){
-			swal('Errou!', 'Falha ao salvar o registro!', 'error');
-		}
-	});//Fim Ajax
-}
